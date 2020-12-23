d2e1d0a6ce4785df4aaaafd715b82e72262cea7a
KalluruS <>
2020-12-23 08:17:45 +0530

    Update terraform-project/terraform-asg/

--- asg.tf
+++ asg.tf
@@ -1,5 +1,16 @@
 resource "aws_launch_template" "asg" {
-  name                    = "${var.COMPONENT}-${var.ENV}"
+  name                    = "${var.COMPONENT}-${var.ENV}-template"
   image_id                = data.aws_ami.ami.id
   instance_type           = var.INSTANCE_TYPE
+}
+
+resource "aws_autoscaling_group" "bar" {
+  name                      = "${var.COMPONENT}-${var.ENV}-asg"
+  max_size                  = 1
+  min_size                  = 1
+  desired_capacity          = 1
+  force_delete              = true
+  launch_template {
+    id                      = aws_launch_template.asg.id
+  }
 }
